79f83b10c5bb32bfe67ad6bfdbfa56337bdc9d8b
Mayank Chopra <makchopra@codeaurora.org>
2012-02-29 12:25:31 +0530

    msm_fb: display: add writeback mode for mddi on 7x30
    Add support for BLT mode in the MDP4.0 overlay architecture while
    downscaling to more than half of the source dimensions to avoid
    underrun issues.
    
    CRs-Fixed: 313524
    Signed-off-by: Jeevan Shriram <jshriram@codeaurora.org>
    Signed-off-by: Mayank Chopra <makchopra@codeaurora.org>
    (cherry picked from commit 33a7d7226c7ab243fe6f3192b5f007435c451c5b)
    
    Change-Id: Ib4086606131db62c8e54b44265ebfbb8f5df91bd
    Signed-off-by: Srivalli Oguri <oguri@codeaurora.org>

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -2180,6 +2180,8 @@ int mdp4_overlay_blt(struct fb_info *info, struct msmfb_overlay_blt *req)
 		mdp4_dsi_video_overlay_blt(mfd, req);
 	else if (ctrl->panel_mode & MDP4_PANEL_LCDC)
 		mdp4_lcdc_overlay_blt(mfd, req);
+	else if (ctrl->panel_mode & MDP4_PANEL_MDDI)
+		mdp4_mddi_overlay_blt(mfd, req);
 
 	mutex_unlock(&mfd->dma->ov_mutex);
 
@@ -2201,6 +2203,8 @@ int mdp4_overlay_blt_offset(struct fb_info *info, struct msmfb_overlay_blt *req)
 		ret = mdp4_dsi_video_overlay_blt_offset(mfd, req);
 	else if (ctrl->panel_mode & MDP4_PANEL_LCDC)
 		ret = mdp4_lcdc_overlay_blt_offset(mfd, req);
+	else if (ctrl->panel_mode & MDP4_PANEL_MDDI)
+		mdp4_mddi_overlay_blt_offset(mfd, req);
 
 	mutex_unlock(&mfd->dma->ov_mutex);
 
@@ -2327,6 +2331,8 @@ static void mdp4_overlay_update_blt_mode(struct msm_fb_data_type *mfd)
 			mdp4_dsi_video_blt_start(mfd);
 		else if (ctrl->panel_mode & MDP4_PANEL_DSI_CMD)
 			mdp4_dsi_overlay_blt_start(mfd);
+		else if (ctrl->panel_mode & MDP4_PANEL_MDDI)
+			mdp4_mddi_overlay_blt_start(mfd);
 	} else {
 		if (mfd->panel_info.type == LCDC_PANEL)
 			mdp4_lcdc_overlay_blt_stop(mfd);
@@ -2334,6 +2340,8 @@ static void mdp4_overlay_update_blt_mode(struct msm_fb_data_type *mfd)
 			mdp4_dsi_video_blt_stop(mfd);
 		else if (ctrl->panel_mode & MDP4_PANEL_DSI_CMD)
 			mdp4_dsi_overlay_blt_stop(mfd);
+		else if (ctrl->panel_mode & MDP4_PANEL_MDDI)
+			mdp4_mddi_overlay_blt_stop(mfd);
 	}
 	mfd->ov0_blt_state = mfd->use_ov0_blt;
 }
@@ -2372,6 +2380,12 @@ static u32 mdp4_overlay_blt_enable(struct mdp_overlay *req,
 		if (mdp4_overlay_validate_downscale(req, mfd, perf_level,
 			clk_rate))
 			use_blt = 1;
+	}
+
+	if (mfd->panel_info.type == MDDI_PANEL) {
+		if ((req->src_rect.h/2) >= req->dst_rect.h ||
+			(req->src_rect.w/2) >= req->dst_rect.w)
+				use_blt = 1;
 	}
 
 	if (mfd->mdp_rev == MDP_REV_41) {
@@ -2500,6 +2514,7 @@ int mdp4_overlay_set(struct fb_info *info, struct mdp_overlay *req)
 					mdp4_set_perf_level();
 			} else if (ctrl->panel_mode & MDP4_PANEL_MDDI) {
 				mdp4_mddi_dma_busy_wait(mfd);
+				mdp4_mddi_blt_dmap_busy_wait(mfd);
 				mdp4_set_perf_level();
 			}
 		} else {
@@ -2550,7 +2565,7 @@ int mdp4_overlay_unset(struct fb_info *info, int ndx)
 #else
 		if (ctrl->panel_mode & MDP4_PANEL_MDDI) {
 			if (mfd->panel_power_on)
-				mdp4_mddi_dma_busy_wait(mfd);
+				mdp4_mddi_blt_dmap_busy_wait(mfd);
 		}
 #endif
 	}
