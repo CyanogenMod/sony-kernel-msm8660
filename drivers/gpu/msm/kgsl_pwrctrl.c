9d33087f30ccf9c48ad4bbfde231ca575b2b4a72
Lynus Vaz <lvaz@codeaurora.org>
2012-04-11 13:12:54 +0530

    msm: kgsl: Stop the device during sleep
    For 7x27, 7x27A and 7x25A, stop the kgsl device during sleep.This
    improved PDT stability results and 72hrs stability on customer devices.
    
    CRs-Fixed: 339805
    Signed-off-by: Lynus Vaz <lvaz@codeaurora.org>
    Signed-off-by: Sudhakara Rao Tentu <srtentu@codeaurora.org>
    (cherry picked from commit fe4bede74b7c767412467f53c827528e3ad6bea4)

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -516,6 +516,7 @@ int kgsl_pwrctrl_init(struct kgsl_device *device)
 	pwr->nap_allowed = pdata->nap_allowed;
 	pwr->idle_needed = pdata->idle_needed;
 	pwr->interval_timeout = pdata->idle_timeout;
+	pwr->strtstp_sleepwake = pdata->strtstp_sleepwake;
 	pwr->ebi1_clk = clk_get(&pdev->dev, "bus_clk");
 	if (IS_ERR(pwr->ebi1_clk))
 		pwr->ebi1_clk = NULL;
@@ -638,7 +639,8 @@ void kgsl_timer(unsigned long data)
 
 	KGSL_PWR_INFO(device, "idle timer expired device %d\n", device->id);
 	if (device->requested_state != KGSL_STATE_SUSPEND) {
-		if (device->pwrctrl.restore_slumber)
+		if (device->pwrctrl.restore_slumber ||
+					device->pwrctrl.strtstp_sleepwake)
 			kgsl_pwrctrl_request_state(device, KGSL_STATE_SLUMBER);
 		else
 			kgsl_pwrctrl_request_state(device, KGSL_STATE_SLEEP);
